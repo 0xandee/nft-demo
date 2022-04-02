86dba8c0dbfeb01742b2a523737161e66b19cbde
Andy Vo <voandy57@gmail.com>
2022-04-02 17:37:06 +0700

    added test_contracts and interfaces

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,5 +1,10 @@
-from scripts.helpful_scripts import get_account, OPENSEA_URL
-from brownie import AdvancedConllectible, network, config
+from scripts.helpful_scripts import (
+    fund_with_link,
+    get_account,
+    OPENSEA_FORMAT,
+    get_contract,
+)
+from brownie import AdvancedCollectible, network, config
 
 sample_token_uri = (
     "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
@@ -8,13 +13,17 @@ sample_token_uri = (
 
 def deploy_and_create():
     account = get_account()
-    advanced_collectible = AdvancedConllectible.deploy(
-        config[network.show_active()]["vrf_coordinator"],
-        config[network.show_active()]["link_token"],
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
         config[network.show_active()]["keyhast"],
         config[network.show_active()]["fee"],
         {"from": account},
     )
+    fund_with_link(advanced_collectible.address)
+    creating_tx = advanced_collectible.createCollectible({"from": account})
+    creating_tx.wait(1)
+    print("New Token has been created")
 
 
 def main():
